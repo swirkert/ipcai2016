794968750c9ec388a3b8b0c8ec902f76f1dd29ea
Sebastian Wirkert <s.wirkert@dkfz-heidelberg.de>
2016-08-19 17:06:54 +0200

    Preparation for Anants configuration file parser.
    Batches shall be created with information for a .ini file in the future.

--- mc/factories.py
+++ mc/factories.py
@@ -21,10 +21,7 @@ Created on Oct 15, 2015
 '''
 
 from mc.tissuemodels import AbstractTissue, GenericTissue
-from mc.batches import AbstractBatch
-from mc.batches import GenericBatch, LessGenericBatch, GenericMeanScatteringBatch
-from mc.batches import ColonMuscleBatch, ColonMuscleMeanScatteringBatch
-from mc.batches import VisualizationBatch
+from mc.batches import AbstractBatch, IniBatch
 
 
 class AbstractMcFactory(object):
@@ -51,65 +48,7 @@ class GenericMcFactory(AbstractMcFactory):
         return GenericTissue()
 
     def create_batch_to_simulate(self):
-        return GenericBatch()
-
-    def __init__(self):
-        '''
-        Constructor
-        '''
-
-
-class LessGenericMcFactory(GenericMcFactory):
-
-    def create_batch_to_simulate(self):
-        return LessGenericBatch()
-
-    def __init__(self):
-        '''
-        Constructor
-        '''
-
-
-class ColonMuscleMcFactory(GenericMcFactory):
-
-    def create_batch_to_simulate(self):
-        return ColonMuscleBatch()
-
-    def __init__(self):
-        '''
-        Constructor
-        '''
-
-
-class GenericMeanScatteringFactory(GenericMcFactory):
-
-    def create_batch_to_simulate(self):
-        return GenericMeanScatteringBatch()
-
-    def __init__(self):
-        '''
-        Constructor
-        '''
-
-
-class ColonMuscleMeanScatteringFactory(GenericMcFactory):
-
-    def create_batch_to_simulate(self):
-        return ColonMuscleMeanScatteringBatch()
-
-    def __init__(self):
-        '''
-        Constructor
-        '''
-
-
-class VisualizationMcFactory(AbstractMcFactory):
-
-    def create_tissue_model(self):
-        return GenericTissue()
-
-    def create_batch_to_simulate(self):
-        return VisualizationBatch()
+        return IniBatch()
 
     def __init__(self):
         '''
